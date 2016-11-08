b30d69caec15c25d60b82b96ed6ba212f7a6dd4a
John Doe <123@mmm.dk>
2016-11-08 13:02:15 +0100

    besked

---
+++ Miniopgaver/Miniopgaver/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Miniopgaver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Opgave1();
+            Opgave2();
+
+        }
+        static void Opgave1()
+        {
+            Debug.WriteLine("Hello");
+            Debug.WriteLine("Anders Assarsson");
+        }
+
+        static void Opgave2()
+        {
+            //opgave 2
+        }
+    }
+}
