60a524dd9a012f6ca5a00909dd13df6445281914
John Doe <123@mmm.dk>
2016-11-08 14:02:13 +0100

    besked2

--- Miniopgaver/Miniopgaver/Program.cs
+++ Miniopgaver/Miniopgaver/Program.cs
@@ -11,10 +11,18 @@ namespace Miniopgaver
     {
         static void Main(string[] args)
         {
+            Debug.WriteLine("------------------------");
+
             Opgave1();
             Opgave2();
+            Opgave3();
+            Opgave4();
+            //Opgave5();
+
+            Debug.WriteLine("------------------------");
 
         }
+     
         static void Opgave1()
         {
             Debug.WriteLine("Hello");
@@ -23,7 +31,30 @@ namespace Miniopgaver
 
         static void Opgave2()
         {
-            //opgave 2
+            int x = 15 + 133;
+            Debug.WriteLine(x);
         }
-    }
+
+        private static void Opgave3()
+        {
+            int x = 24 / 5;
+            Debug.WriteLine(x);
+        }
+
+        private static void Opgave4()
+        {
+            int x = -1 + 3 * 5;
+            Debug.WriteLine(x);
+            int y = (24 + 5) % 7;
+            Debug.WriteLine(y);
+            float z = 15 + -4 * 6 / 11f;
+            Debug.WriteLine(z);
+            float w = 2 + 10 / 6 * 1 - 7 % 2f;
+            Debug.WriteLine(w);
+        }
+
+        private static void Opgave5()
+        {
+
+        }
 }
